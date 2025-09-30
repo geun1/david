eac6fed2aedfa744fb02fdf1bc0448133ed2aa95
gsong <69511382+geun1@users.noreply.github.com>
2025-09-30 19:17:25 +0900

    Add new route for test2 page

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ def menu():
     else:
         hostname = ''
     return render_template("menu.html", computername=hostname)
-
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
 if __name__ == "__main__":
     app.run(debug=True)
